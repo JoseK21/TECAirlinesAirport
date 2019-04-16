f5a8d7b98a634026a501d09da03fe877b70a0fdf
Jose Carlos Nuñez Valverde <josek21nunez@gmail.com>
2019-04-15 20:54:51 -0600

    Fix : Input Validation

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservation.component.css']
 })
 export class ReservationComponent implements OnInit {
+  userCheck:boolean=false;
+  userName:string="";
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * logIn
+   */
+  public logIn(userName:string,password:string) {
+    console.log("User name : "+userName+ "password"+password);
+
+    // Consulta al services
+
+    if(userName.trim() !="" && password.trim()!=""){
+      this.userName=userName;
+      this.logInCard(); // CASO DE PRUEBA - Eliminar luego
+    }else{
+      //Mensage de error...
+    }
+  }
+
+  /**
+   * logInCard
+   */
+  public logInCard() {    
+    this.userCheck=!this.userCheck; // CASO DE PRUEBA
+  }
+
+  /**
+   * pay
+   */
+  public pay(card:string,scode:string,exDate:string) {
+    console.log(card.trim().length);
+    console.log(scode.trim().length);
+    console.log(exDate.trim().length);
+
+    if(card.trim().length===0 || scode.trim().length===0 || exDate.trim().length===0){
+      console.log("ERROR");
+    }
+    else{
+      console.log("card :"+card+ " scode:"+scode+" exDate;"+exDate);
+    }
+  }
+
+  
 }
