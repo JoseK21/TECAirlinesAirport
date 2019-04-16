972f0a2eac1fe9d7d7fa8a8644dbe1103824e567
Jose Carlos Nuñez Valverde <josek21nunez@gmail.com>
2019-04-16 12:12:17 -0600

    Feature: Add registry flight and promotions

--- src/app/promotion/promotion.component.ts
+++ src/app/promotion/promotion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-promotion',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promotion.component.css']
 })
 export class PromotionComponent implements OnInit {
-
+  img:any="assets/image1200400.jpg";
+  dateNOW:string;
+  dateMIN:string;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {   
+    this.dateNOW= formatDate(new Date(), 'yyyy-MM-dd', 'en');
+  }
+
+  /**
+   * setDate
+   */
+  public setDate(dateMin:string) {
+    this.dateMIN=dateMin;    
+  }
+
+  /**
+   * create
+   */
+  public create(Fid:string,DS:string,DE:string,D:string) {
+    console.log(Fid+" "+DS+" "+DE+" "+D+" ");  
+    alert("message here .... ");
+    
+  }
+  public imagePath;
+  imgURL: any;
+  public message: string;
+ 
+  preview(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+      this.img = reader.result; 
+    }
   }
 
 }
--- src/app/register-flight/register-flight.component.ts
+++ src/app/register-flight/register-flight.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-register-flight',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterFlightComponent implements OnInit {
 
+  showModal: number = 0;
+  point: number = 0;
+  dateNOW:string;
+  dateMIN:string;
   constructor() { }
 
   ngOnInit() {
+    this.dateNOW= formatDate(new Date(), 'yyyy-MM-dd', 'en');
+
+  }
+
+  /**
+   * setDate
+   */
+  public setDate(dateMin:string) {
+    this.dateMIN=dateMin;    
+  }
+
+  /**
+   * numModal
+   * 0: pointDeparture
+   * 1: pointArrival
+   * charModal
+   * 
+   */
+  public changeModal(numModal: number, charModal: number) { //CharModal: Input seleccionado
+    if (numModal == 0) {
+      this.point = charModal;
+    }
+    this.showModal = numModal;
   }
 
 }
